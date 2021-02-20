2e8a25f937d8559aeeab2370786f8d2817d8b74c
Kendrick Shaw <kms15@case.edu>
2021-02-20 08:56:16 +0000

    Reuse calculations across dimensions.
    The prior algorithm generated a separate set of samples for each number
    of dimensions, making it O(p * d^2 * r). Because we don't need the
    samples to be independent for each number of dimensions tested, however,
    we can generate a new sum of coordinates for the N-dimensional case
    dimension by merely adding one more term to the (N-1)-dimensional case,
    allowing us to generate the table in O(p * d * r) time instead.

--- distance.c
+++ distance.c
@@ -69,11 +69,55 @@ int main( int argc, char **argv )
         }
     }
 
+
     // Initialize random seed
     srand(time(0));
 
+    // Initialize totals to zero
+    double totals[Dimensions+1][Powers];
+    int d,p;
+    for (d=0; d <= Dimensions; ++d) {
+        for (p=0; p<Powers; ++p) {
+            totals[d][p] = 0.;
+        }
+    }
+
+    // zero out the first row (i.e. zero dimensional case) of the sums
+    double sums[Dimensions+1][Powers];
+    for (p=0; p<Powers; ++p) {
+        sums[0][p] = 0.;
+    }
+
+    // Generate and add up sums of coordinate differences at various dimensions
+    // from two randomly generated points in the hypercube.
+    for (long r = 0; r < Randoms; ++r) {
+        // fill in the sum of powers for a single sample at various dimensions
+        // and powers.
+        for (d=1; d <= Dimensions; ++d) {
+            // For each dimension, get 2 coordinates in this dimension.
+            // we only care about dx, the delta in the coordinate
+            // use abs value for odd powers calculation
+            double dx, dx_cumprod;
+            dx = fabs((rand() - rand()) * scale);
+
+            // for each power, the sum will be the entry from the row above
+            // plus dx raised to that power.
+            double cumprod = 1;
+            for (p=0; p<Powers; ++p) {
+                cumprod *= dx;
+                sums[d][p] = sums[d-1][p] + cumprod;
+            }
+        }
+
+        // Add the pth root of each sum to the totals
+        for (d=1; d <= Dimensions; ++d) {
+            for (p=0; p<Powers; ++p) {
+                totals[d][p] += pow(sums[d][p], 1./(p+1));
+            }
+        }
+    }
+
     // Title line
-    int p ;
     printf("DIM\\Power, ");
     for (p=1;p<=Powers;++p) {
         printf("%d, ",p);
@@ -81,65 +125,25 @@ int main( int argc, char **argv )
     printf("\n");
 
     // Loop though dimensions
-    int d ;
-    for ( d=1 ; d <= Dimensions ; ++d ) {
-        double sum[Powers] ;
-        int p ;
-
+    for (d=1; d <= Dimensions; ++d) {
         // Start line with dimension
         printf("%d, ",d);
 
-        // zero out sums
+        // Print out the distances
         for (p=0;p<Powers;++p) {
-            sum[p] = 0. ;
-        }
-
-        // Random segments (i.e. 2 points of increasing dimension)
-        long r ;
-        for (r = 0 ; r < Randoms ; ++r ) {
-
-            // Zero out segment
-            double seg[Powers];
-            for (p=0;p<Powers;++p) {
-                seg[p] = 0. ;
-            }
-
-            // Add up for this dimension
-            int dd ;
-            for (dd = 0 ; dd < d ; ++dd) {
-                double sx = 1. ;
-                // For each dimension, get 2 coordinates in this dimension -- rand() and rand() scaled
-                // we only care about dx, the delta in the coordinate
-                // use abs value for odd powers calculation
-                double dx = fabs((rand() - rand()) * scale ) ;
-                // compute increasing powers of dx the easy way
-                for (p=0;p<Powers;++p) {
-                    sx *= dx ;
-                    seg[p] += sx ;
+            if (Normalize) {
+                for (p=0;p<Powers;++p){
+                    // note p is 0-indexed in C, but 1-indexed for calculation
+                    printf("%g, ", totals[d][p]/Randoms/pow(d,1/(1.+p)));
+                }
+            } else {
+                for (p=0;p<Powers;++p){
+                    printf("%g, ", totals[d][p]/Randoms);
                 }
             }
-
-            // Add segments to sum, taking the appropriate root
-            sum[0] += seg[0] ; // Manhattan or Taxi
-            sum[1] += sqrt(seg[1]) ; // Euclidean
-            for (p=2;p<Powers;++p) { // this is why Powers must be 3 or higher
-                // note p is 0-indexed in C, but 1-indexed for calculation
-                sum[p] += pow(seg[p],1/(1.+p));
-            }
+            // finish line
+            printf("\n");
         }
-
-        if (Normalize) {
-            for (p=0;p<Powers;++p){
-                // note p is 0-indexed in C, but 1-indexed for calculation
-                printf("%g, ",sum[p]/Randoms/pow(d,1/(1.+p)));
-            }
-        } else {
-            for (p=0;p<Powers;++p){
-                printf("%g, ",sum[p]/Randoms);
-            }
-        }
-        // finish line
-        printf("\n");
     }
 
     // success
